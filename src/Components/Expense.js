c2ffb79339940f57a14607cd814826ef39c631c2
Harshit Bareja <hbareja.07@gmail.com>
2024-07-09 13:00:42 +0530

    showing expenses

--- src/Components/Expense.js
+++ src/Components/Expense.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const Expense = () => {
+  const {credit, debit} = useSelector(state => state.expenses)
+
+  
+
+  const totalCredit = credit.reduce((acc, curr)=>{
+    return acc+curr;
+  }, 0)
+  const totalDebit = debit.reduce((acc, curr)=>{
+    return acc+curr;
+  }, 0)
+  const totalExpense = totalCredit + totalDebit;
   return (
     <div className='expense-container'>
-      <div className='Credit'></div>
-      <div className='Debit'></div>
-      <div className='Total'></div>
+      <div className='Credit'>{totalCredit}</div>
+      <div className='Debit'>{totalDebit}</div>
+      <div className='Total'>{totalExpense}</div>
     </div>
   )
 }
